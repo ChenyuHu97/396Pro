98d3904b511c5d7871caaef8d6b7c35668500f85
ChenyuHu97 <77149825+ChenyuHu97@users.noreply.github.com>
2021-05-27 00:36:18 +0800

    Add files via upload

---
+++ FrontEndIndex/js/js1.js
@@ -0,0 +1,20 @@
+var flag=false;
+function show_menu(){
+		var title1=document.getElementById("title");
+		if(flag){
+			title1.style.display="none";
+			flag=false;
+		}else{
+			title1.style.display="block";
+			flag=true;
+		}
+}
+function show_menu1(){
+		var title1=document.getElementById("title");
+			title1.style.display="none";
+			flag=false;
+}
+
+
+
+
